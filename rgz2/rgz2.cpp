e68e9d1d9cf8ce24a0f203853b286fd9b34919fb
KirillOleinikovJ <115183797+KirillOleinikovJ@users.noreply.github.com>
2023-06-11 21:25:07 +0200

    only console

--- rgz2/rgz2.cpp
+++ rgz2/rgz2.cpp
@@ -1,90 +1,12 @@
-//Гра відбувається на прямокутному полі. Розмір поля вибирається гравцем. Поле складається з клітин. У кожної клітини є 8 сусідів. Правила такі:
-//-жива клітина, у якій є менше ніж дві живі клітини серед сусідів, вмирає.
-//- жива клітина, у якій більш ніж три живі клітини серед сусідів, також вмирає.
-//- жива клітина, у якій дві або три живі клітини серед сусідів, продовжує жити.
-//- мертва клітина, у якій рівно три живі клітини серед сусідів, стає живою.
-//Початкова конфігурація задається гравцем.Гра закінчується тоді, 
-//коли на полі немає більше живих клітин, інакше розподіл клітин повторюється по кроках.
-//На кожному кроці відображається поле, що складається з клітин.
-//Кожна клітина відображається цифрою або кольором(якщо готові зробити графічне представлення), 
-//що відповідає стану клітини.
+﻿
+
 #include <iostream>
-#include<fstream>
-#include<SFML/Graphics.hpp>//ссылка на либу: https://www.sfml-dev.org/index.php 
-#include<conio.h>
-// туториал по подключению либы в visual studio: https://www.sfml-dev.org/tutorials/2.5/start-vc.php
-using namespace sf;
-class Field
+
+int main()
 {
-public:
-    int width; //ширина карты
-    int height; //высота карты
-    int sizeTile; //размер каждого тайла
-    bool** world; //двумерный массив с клетками в этом ходе
-    bool** nextStep; //двумерный массив с клетками которые будут в следующем ходе
-    int neighbours; //счетчик отвечающий за количество соседей
-    float interval;//интервал между прорисовками окна
-    RenderWindow window;//графическое окно
-    Clock clock; float time;// я хз что это, вроде для интервала надо
-    RectangleShape tileBlack; //тайлы клеток
-    char mode;//выбор версии игры (консольная/графическая)
-    Field();
-    void Game_Cycle_Console();//консольная версия игры
-    void Game_Cycle_Graphic();//графическая
-    void Clear_Window();//перерисовка графического окна (на каждом ходу)
-    void User_Input_Param(); //нужно дописать ввод матрицы юзером
-    void File_Input_Param();//ввод матрицы из файла
-    void Set_Mode() { std::cin >> mode; }//ну тут понятно вроде
-    void Print_Console();//принт матрицы в консоль
-    //можно добавить больше сеттеров для отдельніх полей класса а так я хз что ещё тут надо
-    ~Field()//деструктор удаляет двумерные динамические массивы
-    {
-        for (int i = 0; i < width; i++)
-        {
-            delete[] world[i];
-            delete[] nextStep[i];
-        }
-        delete[]world;
-        delete[]nextStep;
-    }
-};
-Field::Field()//конструктор, задаёт динамические массивы и дефолтные значения полей, задаёт размер и цвет тайлам
-{
-    width = 100;
-    height = 50;
-    sizeTile = 10;
-    interval = 0;
-    world = new bool* [width];
-    nextStep = new bool* [width];
-    for (int i = 0; i < width; i++)
-    {
-        world[i] = new bool[height];
-        nextStep[i] = new bool[height];
-    }
-    for (int i = 0; i < width; i++)
-    {
-        for (int j = 0; j < height; j++)
-        {
-            world[i][j] = 0;
-            nextStep[i][j] = 0;
-        }
-    }
-    this->tileBlack.setSize(Vector2f(sizeTile, sizeTile));//черный квадрат будет рисоватся где живые тоесть заполненные клетки
-    tileBlack.setFillColor(Color::Black);
+    std::cout << "Hello World!\n";
 }
-void Field::Game_Cycle_Console()
-{
-    std::cout << "Enter 1 to input matrix manualy or 2 to take it from file\n";
-    char c;
-    std::cin >> c;
-    if (c == '1') this->User_Input_Param();
-    else if(c=='2') this->File_Input_Param();// ввод матрицы из файла, можно заменить на ввод юзером
-    std::cout << "\nGame starts:\n";
-    for (int i = 0; i < width; i++, std::cout << std::endl) 
-    {
-        for (int j = 0; j < height; j++)
-            std::cout << world[i][j] << "\t";
-    }
+
     std::cout << std::endl;
     while (true)
     {
